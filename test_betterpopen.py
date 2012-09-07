716e203932ff815f09db74b4c4ac4d88339f332d
Joshua T Corbin <jcorbin@wunjo.org>
2012-09-07 08:08:57 -0400

    Contain better popen test cases

--- test_betterpopen.py
+++ test_betterpopen.py
@@ -4,30 +4,33 @@ import subprocess
 
 from betterpopen import Popen
 
-proc = Popen('false')
-assert proc.wait() == 1
-assert proc.rusage is not None
+def test_wait():
+    proc = Popen('false')
+    assert proc.wait() == 1
+    assert proc.rusage is not None
 
-assert Popen('true').check() == 0
-assert Popen('false').check({1}) == 1
+def test_check():
+    assert Popen('true').check() == 0
+    assert Popen('false').check({1}) == 1
 
-try:
-    Popen('false').check()
-except subprocess.CalledProcessError:
-    pass
-else:
-    assert False, "shoud've raised subprocess.CalledProcessError"
+    try:
+        Popen('false').check()
+    except subprocess.CalledProcessError:
+        pass
+    else:
+        assert False, "shoud've raised subprocess.CalledProcessError"
 
-assert Popen('gzip -c /dev/urandom',
-    stdout=open('/dev/null', 'w'),
-    rlimits={
-        resource.RLIMIT_CPU: (2, 4),
-    }
-).wait() == -signal.SIGXCPU
+def test_rlimits():
+    assert Popen('gzip -c /dev/urandom',
+        stdout=open('/dev/null', 'w'),
+        rlimits={
+            resource.RLIMIT_CPU: (2, 4),
+        }
+    ).wait() == -signal.SIGXCPU
 
-assert Popen('gzip -c /dev/urandom',
-    stdout=open('/dev/null', 'w'),
-    rlimits={
-        resource.RLIMIT_CPU: (2, 2),
-    }
-).wait() == -signal.SIGKILL
+    assert Popen('gzip -c /dev/urandom',
+        stdout=open('/dev/null', 'w'),
+        rlimits={
+            resource.RLIMIT_CPU: (2, 2),
+        }
+    ).wait() == -signal.SIGKILL
