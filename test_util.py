b2b337f70126036d67f39cfff54049049dd96d5d
Joshua T Corbin <jcorbin@wunjo.org>
2012-09-08 12:40:41 -0400

    Kill outstanding pids atexit in test_util

--- test_util.py
+++ test_util.py
@@ -1,12 +1,19 @@
+import errno
 import os
+import signal
 import sys
 from functools import wraps
 
+from collections import defaultdict
+Outstanding = defaultdict(lambda: [])
+
 def fork_run(run, *args, **kwargs):
+    global Outstanding
     pid = os.fork()
     if pid == 0:
         run(*args, **kwargs)
         sys.exit(255)
+    Outstanding[os.getpid()].append(pid)
     return pid
 
 def forks(f):
@@ -14,3 +21,24 @@ def forks(f):
     def forker(*args, **kwargs):
         return fork_run(f, *args, **kwargs)
     return forker
+
+def kill_pids():
+    global Outstanding
+    pids = Outstanding[os.getpid()]
+    signal.signal(signal.SIGCLD, signal.SIG_DFL)
+    killed = set()
+    for pid in pids:
+        try:
+            os.kill(pid, 0)
+        except OSError as err:
+            if err.errno != errno.ESRCH:
+                print >>sys.stderr, 'while trying to cleanup pid', pid, 'got', err
+            continue
+        os.kill(pid, signal.SIGKILL)
+        killed.add(pid)
+    while killed:
+        pid, status = os.wait()
+        killed.remove(pid)
+
+import atexit
+atexit.register(kill_pids)
