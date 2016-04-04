322b487dc663bcac8d30540f7e954fd381d8e1fa
Joshua T Corbin <jcorbin@wunjo.org>
2016-04-04 13:45:16 -0700

    Add as_exited

--- __init__.py
+++ __init__.py
@@ -2,3 +2,30 @@ from betterpopen import Popen
 from killer import Killer
 from obituary import obituary
 from reaper import Reaper, AsyncReaper, ProcessRegistry
+
+import signal
+
+
+def as_exited(procs, limit=40):
+    pending = []
+    while True:
+        still = []
+        for proc in pending:
+            if proc.returncode is not None:
+                yield proc
+            else:
+                still.append(proc)
+        pending = still
+        while len(pending) < limit and procs is not None:
+            try:
+                proc = next(procs)
+            except StopIteration:
+                procs = None
+            else:
+                if proc.returncode:
+                    yield proc
+                else:
+                    pending.append(proc)
+        if not len(pending):
+            break
+        signal.pause()
